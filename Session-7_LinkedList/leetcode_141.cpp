0e1aade9938b34cf0ea01a2e5de49cb00e53fc66
Disha <24BCS10773@cuchd.in>
2026-01-21 20:51:51 +0530

    Create leetcode_141.cpp

---
+++ Session-7_LinkedList/leetcode_141.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            if(fast->next==slow){
+                return true;
+            }
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        return false;
+    }
+};
